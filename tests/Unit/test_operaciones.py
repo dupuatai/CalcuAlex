6ded86582de39abe7904bd3dbe4879c2a27d13c7
dupuatai <dupuatai@gmail.com>
2025-12-11 20:06:00 -0600

    Agregar workflows

---
+++ tests/Unit/test_operaciones.py
@@ -0,0 +1,23 @@
+import pytest
+from calculadora import sumar, restar, multiplicar, dividir
+
+
+def test_sumar():
+    assert sumar(2, 3) == 5
+
+
+def test_restar():
+    assert restar(5, 2) == 3
+
+
+def test_multiplicar():
+    assert multiplicar(4, 3) == 12
+
+
+def test_dividir():
+    assert dividir(10, 2) == 5
+
+
+def test_dividir_por_cero():
+    with pytest.raises(ValueError):
+        dividir(10, 0)
